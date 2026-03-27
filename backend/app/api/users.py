8c60b0a34d2ae1c390705c0899470752f8ea3c92
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2026-03-27 16:17:46 -0400

    Adding toggle follow functionality

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,7 +1,8 @@
 # User profiles, follows
 from uuid import UUID
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from app.crud import crud_user
 from app.schemas.user import UserResponse, UserUpdate
 from app.schemas.article import ArticleCreate, ArticleResponse
 from app.db.database import get_db
@@ -27,15 +28,16 @@ def get_profile(username: str, db: Session = Depends(get_db)):
 def get_user_articles(username: str, db: Session = Depends(get_db)):
     pass
 
-# Follow user
-@router.post("/{username}/follow")
-def follow_user(username: str, db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_id)):
-    pass
-
 # Unfollow user
-@router.delete("/{username}/follow")
-def unfollow_user(username: str, db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_id)):
-    pass
+@router.post("/{username}/follow")
+def toggle_follow(username: str, db: Session = Depends(get_db), current_user_id: UUID = Depends(get_current_user_id)):
+    target_user = crud_user.get_user_by_username(db, username)
+
+    if not target_user:
+        raise HTTPException(status_code=404, details="User not found")
+
+    response = crud_user.toggle_follow(db, target_user.id, user_id=current_user_id)
+    return response
 
 # Get all liked posts for user
 @router.get("/{username}/likes")
--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from uuid import UUID
-
+from app.models.associations import user_follows
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
 from app.core.security import hash_password 
@@ -9,6 +9,9 @@ def get_user_by_id(db: Session, user_id: UUID):
     # Query the database for a user matching this exact UUID
     users = db.query(User).filter(User.id == user_id)
     return users.first()
+
+def get_followers(db: Session, user_id: UUID):
+    pass
 
 def get_user_by_email(db: Session, email: str):
     # Required to check if an email is already registered during signup
@@ -31,3 +34,41 @@ def create_user(db: Session, user: UserCreate):
     db.refresh(new_user)    
     # 4. Return the new user object
     return new_user
+
+# Follow/unfollow a user
+def toggle_follow(db: Session, other_user_id: UUID, user_id: UUID):
+    # Prevent self-following
+    if user_id == other_user_id:
+        return {"message": "You cannot follow yourself", "followed": False}
+    
+    # Check if user already follows
+    following = db.query(user_follows).filter(
+        user_follows.c.follower_id == user_id,
+        user_follows.c.followed_id == other_user_id
+    )
+
+    already_following = following.first()
+
+    # If user already follows
+    if already_following:
+        # Unfollow
+        statement = user_follows.delete().where(
+            (user_follows.c.follower_id==user_id) &
+            (user_follows.c.followed_id==other_user_id)
+        )
+        message = "Person unfollowed"
+        followed_status = False
+    else: # User hasn't like the article
+        # Like it
+        statement = user_follows.insert().values(
+            follower_id=user_id,
+            followed_id=other_user_id
+        )
+        message = "Person followed"
+        followed_status = True
+    
+    db.execute(statement)
+    db.commit()
+
+    # Return the new state so the frontend knows what to display
+    return {"message": message, "followed": followed_status} 
