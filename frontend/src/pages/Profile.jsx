5b0c116cfa1ebc92c3d550c3f3c67775f3b124ce
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2026-04-01 11:22:30 -0400

    Oops

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -22,11 +22,6 @@ export default function Profile() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [following, setFollowing] = useState(false);
-  const [modalConfig, setModalConfig] = useState({
-    isOpen: false,
-    title: "",
-    endpoint: "",
-  });
 
   useEffect(() => {
     const fetchAllProfileData = async () => {
@@ -90,27 +85,6 @@ export default function Profile() {
     } catch (error) {
       console.error("Follow Failed:", error);
     }
-  };
-
-  // Set modal config
-  const openFollowers = () => {
-    setModalConfig({
-      isOpen: true,
-      title: "Followers",
-      endpoint: `${BASE_URL}/users/${username}/followers`,
-    });
-  };
-
-  const openFollowing = () => {
-    setModalConfig({
-      isOpen: true,
-      title: "Following",
-      endpoint: `${BASE_URL}/users/${username}/following`,
-    });
-  };
-
-  const closeModal = () => {
-    setModalConfig({ ...modalConfig, isOpen: false });
   };
 
   if (isLoading) {
